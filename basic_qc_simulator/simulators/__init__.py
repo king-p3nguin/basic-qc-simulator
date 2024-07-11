6328ac3d0ce0fdbd659910ff21d882e616735955
Kazuki Tsuoka <kazukitsuoka@g.ecc.u-tokyo.ac.jp>
2024-07-11 20:07:13 +0900

    feat add state vector to MPS conversion method

--- basic_qc_simulator/simulators/__init__.py
+++ basic_qc_simulator/simulators/__init__.py
@@ -3,4 +3,5 @@ This module contains the simulators for the basic quantum computing simulator.
 """
 
 from .density_matrix import DensityMatrixSimulator
+from .matrix_product_state import MatrixProductStateSimulator
 from .state_vector import StateVectorSimulator
--- basic_qc_simulator/simulators/matrix_product_state.py
+++ basic_qc_simulator/simulators/matrix_product_state.py
@@ -14,30 +14,30 @@ logger = logging.getLogger(__name__)
 
 class MatrixProductStateSimulator(AbstractSimulator):
     """
-    Class for the state vector simulator
+    Class for the matrix product state simulator
     """
 
     def _prepare_state(self, num_qubits: int) -> np.ndarray:
-        """Prepare the initial state vector
+        """Prepare the initial matrix product state
 
         Args:
             num_qubits (int): number of qubits
 
         Returns:
-            np.ndarray: initial state vector
+            np.ndarray: initial matrix product state
         """
         raise NotImplementedError
 
     @staticmethod
     def _apply_gate(instruction: Instruction, state: np.ndarray) -> np.ndarray:
-        """Apply a gate to the state vector
+        """Apply a gate to the matrix product state
 
         Args:
             instruction (Instruction): gate instruction
-            state (np.ndarray): state vector
+            state (np.ndarray): matrix product state
 
         Returns:
-            np.ndarray: resulting state vector
+            np.ndarray: resulting matrix product state
         """
         raise NotImplementedError
 
@@ -46,6 +46,40 @@ class MatrixProductStateSimulator(AbstractSimulator):
 
         Args:
             save_resut_dict (dict): dictionary with the saving result information
-            state (np.ndarray): state vector to save
+            state (np.ndarray): matrix product state to save
         """
         raise NotImplementedError
+
+    @staticmethod
+    def _state_vector_to_matrix_product_state(state_vector: np.ndarray) -> np.ndarray:
+        """Convert a state vector to a matrix product state
+
+        Args:
+            state_vector (np.ndarray): state vector
+
+        Returns:
+            np.ndarray: matrix product state
+        """
+        # pylint: disable=invalid-name
+        assert state_vector.ndim == 1
+        num_qubits = int(np.log2(state_vector.shape[0]))
+        arrs = []
+
+        r = 1
+        for i in range(num_qubits - 1):
+            # Reshape the state vector to a matrix
+            #     2   2   2   2   2   2
+            #  ┌┴─┴─┴─┴─┴─┴┐        ┌───────────┐
+            #  │                      │ →  	─┤                      ├─
+            #  └───────────┘        └───────────┘
+            state_vector = state_vector.reshape(2 * r, 2 ** (num_qubits - i - 1))
+            U, S, V_dagger = np.linalg.svd(state_vector, full_matrices=False)
+            tmp_A = U @ np.diag(S)
+            state_vector = V_dagger
+            r = tmp_A.shape[1]
+            col = tmp_A.shape[0] // 2
+            arrs.append([tmp_A[:col, :], tmp_A[col : 2 * col, :]])
+
+        col2 = state_vector.shape[1] // 2
+        arrs.append([state_vector[:, :col2], state_vector[:, col2 : 2 * col2]])
+        return arrs
